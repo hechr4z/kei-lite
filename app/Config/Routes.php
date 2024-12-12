e08a4536a8a7144ad806455d0954af07dbe91c79
Tio Ramadhani <tioramadhani654@gmail.com>
2024-12-12 14:24:32 +0700

    tambah route dan controller untuk fitur kategori induk, produk, meta

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -376,5 +376,21 @@ $routes->group('', ['filter' => 'admin'], function ($routes) {
     $routes->post('/admin-update-tentang-kami/(:num)', 'KomunitasEkspor::update_admin_tentang_kami/$1');
 
     // Kategori Induk
-    $routes->get('/admin-kategori-induk', 'KomunitasEkspor:kategori_induk');
+    $routes->get('/admin-kategori-induk', 'KomunitasEkspor::admin_kategori_induk');
+    $routes->get('/admin-kategori-induk-tambah', 'KomunitasEkspor::admin_kategori_induk_create');
+    $routes->post('/admin-kategori-induk-store', 'KomunitasEkspor::admin_kategori_induk_store/$1');
+    $routes->get('/admin-kategori-induk-edit/(:num)', 'KomunitasEkspor::admin_kategori_induk_edit/$1');
+    $routes->post('/admin-kategori-induk-update/(:num)', 'KomunitasEkspor::admin_kategori_induk_update/$1');
+    $routes->get('/admin-kategori-induk-delete/(:num)', 'KomunitasEkspor::admin_kategori_induk_destroy/$1');
+
+    // Kategori Produk
+    $routes->get('/admin-kategori-produk', 'KomunitasEkspor::admin_kategori_produk');
+    $routes->get('/admin-kategori-produk-tambah', 'KomunitasEkspor::admin_kategori_produk_create');
+    $routes->post('/admin-kategori-produk-store', 'KomunitasEkspor::admin_kategori_produk_store/$1');
+    $routes->get('/admin-kategori-produk-edit/(:num)', 'KomunitasEkspor::admin_kategori_produk_edit/$1');
+    $routes->post('/admin-kategori-produk-update/(:num)', 'KomunitasEkspor::admin_kategori_produk_update/$1');
+    $routes->get('/admin-kategori-produk-delete/(:num)', 'KomunitasEkspor::admin_kategori_produk_destroy/$1');
+
+    // Meta
+    $routes->get('/admin-meta', 'KomunitasEkspor::admin_meta');
 });
