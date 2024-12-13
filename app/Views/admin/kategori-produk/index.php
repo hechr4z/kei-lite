a561c0ac41ccd1520dd79af40bde75d29a283eb5
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-12-13 10:37:22 +0700

    Refactor admin kategori-produk/index.php to display category data with pagination

--- app/Views/admin/kategori-produk/index.php
+++ app/Views/admin/kategori-produk/index.php
@@ -66,29 +66,42 @@
                                 <thead>
                                     <tr>
                                         <th class="text-center" valign="middle">No</th>
-                                        <th class="text-center" valign="middle">Kategori Produk Indonesia</th>
-                                        <th class="text-center" valign="middle">Kategori Produk English</th>
+                                        <th class="text-center" valign="middle">Kategori Induk</th>
+                                        <th class="text-center" valign="middle">Kategori Produk ID</th>
+                                        <th class="text-center" valign="middle">Kategori Produk EN</th>
                                         <th class="text-center" valign="middle">Aksi</th>
                                     </tr>
                                 </thead>
-                                <tbody>
-                                    <?php $no = 1; ?>
-                                    <?php foreach ($kategori_produk as $item): ?>
+                                <?php if (empty($kategori_produk)): ?>
+                                    <tbody>
                                         <tr>
-                                            <td class="text-center" valign="middle"><?= $no++; ?></td>
-                                            <td class="text-center" valign="middle"><?= $item['nama_kategori_produk']; ?></td>
-                                            <td class="text-center" valign="middle"><?= $item['nama_kategori_produk_en']; ?></td>
-                                            <td class="text-center align-middle">
-                                                <div class="d-flex justify-content-center align-items-center">
-                                                    <a href="<?= base_url('/admin-kategori-produk-delete/' . $item['id_kategori_produk']) ?>" class="btn btn-sm text-white me-2" style="background-color: #F2BF02;">Hapus</a>
-                                                    <a href="<?= base_url('/admin-kategori-produk-edit/' . $item['id_kategori_produk']) ?>" class="btn btn-sm text-white" style="background-color: #03AADE;">Ubah</a>
-                                                </div>
-                                            </td>
+                                            <td colspan="5" class="text-center">Masih belum ada Data Kategori Produk.</td>
                                         </tr>
-                                    <?php endforeach; ?>
-                                </tbody>
+                                    </tbody>
                             </table>
-
+                        <?php else: ?>
+                            <tbody>
+                                <?php $start = ($page - 1) * $perPage + 1; ?>
+                                <?php foreach ($kategori_produk as $item): ?>
+                                    <tr>
+                                        <td class="text-center" valign="middle"><?= $start++; ?></td>
+                                        <td class="text-center" valign="middle"><?= $item['kategori_induk']; ?></td>
+                                        <td class="text-center" valign="middle"><?= $item['nama_kategori_produk']; ?></td>
+                                        <td class="text-center" valign="middle"><?= $item['nama_kategori_produk_en']; ?></td>
+                                        <td class="text-center align-middle">
+                                            <div class="d-flex justify-content-center align-items-center">
+                                                <a href="<?= base_url('/admin-kategori-produk-delete/' . $item['id_kategori_produk']) ?>" class="btn btn-sm text-white me-2" style="background-color: #F2BF02;">Hapus</a>
+                                                <a href="<?= base_url('/admin-kategori-produk-edit/' . $item['id_kategori_produk']) ?>" class="btn btn-sm text-white" style="background-color: #03AADE;">Ubah</a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                            </table>
+                            <div class="mt-2">
+                                <?= $pager->links('default', 'bootstrap_pagination') ?>
+                            </div>
+                        <?php endif; ?>
                         </div><!--//table-responsive-->
                     </div><!--//app-card-body-->
                 </div><!--//app-card-->
