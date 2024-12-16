7123a92d671bb31f9ef1990afcf910a6cd4c5d4e
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-12-16 11:15:13 +0700

    admin keuntungan add views

---
+++ app/Views/admin/keuntungan-daftar/add.php
@@ -0,0 +1,46 @@
+<?= $this->extend('admin/template/template'); ?>
+<?= $this->section('content'); ?>
+
+<div class="app-content pt-3 p-md-3 p-lg-4">
+    <div class="container-xl">
+        <h1 class="app-page-title" style="color: #03AADE;">Tambah Data Keuntungan Daftar</h1>
+        <hr class="mb-4">
+        <div class="row g-4 settings-section">
+            <div class="col-12 col-md-8">
+                <div class="app-card app-card-settings shadow-sm p-4">
+                    <div class="card-body">
+                        <form action="<?= base_url('admin-create-keuntungan') ?>" method="post" enctype="multipart/form-data">
+                            <div class="mb-3">
+                                <label class="form-label">Judul Keuntungan ID</label>
+                                <input type="text" class="form-control" name="judul_keuntungan" placeholder="Masukkan Judul Keuntungan ID" required>
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="form-label">Judul Keuntungan EN</label>
+                                <input type="text" class="form-control" name="judul_keuntungan_en" placeholder="Masukkan Judul Keuntungan EN" required>
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="form-label">Deskripsi Keuntungan ID</label>
+                                <input type="text" class="form-control" name="deskripsi_keuntungan" placeholder="Masukkan Deskripsi Keuntungan ID" required>
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="form-label">Deskripsi Keuntungan EN</label>
+                                <input type="text" class="form-control" name="deskripsi_keuntungan_en" placeholder="Masukkan Deskripsi Keuntungan EN" required>
+                            </div>
+
+                            <div class="mb-3">
+                                <button type="submit" class="btn text-white" style="background-color: #03AADE;">Tambah</button>
+                                <a href="<?= base_url('admin-keuntungan') ?>" class="btn btn-secondary">Kembali</a>
+                            </div>
+                        </form>
+                    </div>
+                </div><!--//app-card-->
+            </div>
+        </div><!--//row-->
+        <hr class="my-4">
+    </div><!--//container-fluid-->
+</div><!--//app-content-->
+
+<?= $this->endSection('content') ?>
